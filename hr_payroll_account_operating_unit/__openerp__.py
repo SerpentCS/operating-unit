01ba51a5f651f783001fb00bd6bb99c534560f1e
SerpentCS <support@serpentcs.com>
2016-08-29 18:07:33 +0530

    [ADD] added Readme file and minor fixes

--- hr_payroll_account_operating_unit/__openerp__.py
+++ hr_payroll_account_operating_unit/__openerp__.py
@@ -8,14 +8,13 @@
     "name": "HR Payroll Account Operating Unit",
     "version": "8.0.1.0.0",
     "license": 'AGPL-3',
-    "author": "Eficent",
+    "author": "Eficent Business and IT Consulting Services S.L., "
+              "Serpent Consulting Services Pvt. Ltd.,"
+              "Odoo Community Association (OCA)",
+    "website": "http://www.eficent.com",
     "category": "Generic Modules/Human Resources",
-    "depends": ["hr_payroll_account", "hr_contract_operating_unit","account_operating_unit"],
-    "description": """
-HR Payroll Account Operating Unit
-=================================
-Adds a the operating unit to the account moves created by the payslip,
-based on the employee's Operating Unit defined in the Contract.
-    """,
+    "depends": ["hr_payroll_account",
+                "hr_contract_operating_unit",
+                "account_operating_unit"],
     'installable': True,
 }
