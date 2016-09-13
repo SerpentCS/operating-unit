c2fe368a40b5f4222738c998050cd894f2669e19
SerpentCS <support@serpentcs.com>
2016-09-13 16:17:13 +0530

    [ADD] Added Test Cases.

--- hr_payroll_account_operating_unit/tests/__init__.py
+++ hr_payroll_account_operating_unit/tests/__init__.py
@@ -3,5 +3,4 @@
 # Jordi Ballester Alomar
 # © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from . import models
-from . import tests
+from . import test_payroll_account_operating_unit
