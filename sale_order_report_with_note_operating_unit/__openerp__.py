a029f6f703d893363e4e60b7ff96d083a42ed395
SerpentCS <support@serpentcs.com>
2016-09-22 17:36:51 +0530

    [ADD] Added the module.

---
+++ sale_order_report_with_note_operating_unit/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © 2015 Eficent Business and IT Consulting Services S.L.
+# - Jordi Ballester Alomar
+# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Sales Order Report with Note Operating Unit',
+    'version': '8.0.1.0.0',
+    'category': 'Reports/Qweb',
+    'license': 'AGPL-3',
+    "author": "Eficent, "
+              "Serpent CS,"
+              "Odoo Community Association (OCA)",
+    'website': "https://odoo-community.org/",
+    'depends': ['sale_order_report_with_note',
+                'sale_operating_unit'],
+    'data': [
+              'views/report_saleorder_qweb.xml'
+              ],
+    'installable': True,
+}
